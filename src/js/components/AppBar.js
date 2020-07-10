2ae4a447c2fe4b2519953cde774631e0f327a0c8
Anugerah Erlaut <aerlaut@live.com>
2020-07-10 18:03:54 +0700

    adding push state capability

--- src/js/components/AppBar.js
+++ src/js/components/AppBar.js
@@ -7,9 +7,21 @@ export default class AppBar extends HTMLElement {
   }
 
   connectedCallback() {
+    // Enable sidebar
     document.addEventListener("DOMContentLoaded", function () {
       let elems = document.querySelectorAll(".sidenav");
       M.Sidenav.init(elems);
+    });
+
+    // Enable push API for links
+    const links = document.querySelectorAll(".navigation a");
+
+    links.forEach((link) => {
+      link.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        history.pushState(null, null, e.target.href);
+      });
     });
   }
 }
