6b4353666e2a854738924417ddd078e925bffa9f
Anugerah Erlaut <aerlaut@live.com>
2020-07-10 15:39:34 +0700

    make home page

--- src/js/components/AppBar.js
+++ src/js/components/AppBar.js
@@ -1,28 +1,9 @@
+import AppBarHtml from "html/templates/app-bar.html";
+
 export default class AppBar extends HTMLElement {
   constructor() {
     super();
-
-    this.innerHTML = `
-                <nav>
-                <div class="nav-wrapper">
-                    <a href="#" data-target="slide-out" class="sidenav-trigger">☰</span></a>
-                    <a href="#" class="brand-logo">Liga Bola</a>
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">JavaScript</a></li>
-                    </ul>
-                    </div>
-                </nav>
-
-                <ul id="slide-out" class="sidenav">
-                    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-                    <li><a href="#!">Second Link</a></li>
-                    <li><div class="divider"></div></li>
-                    <li><a class="subheader">Subheader</a></li>
-                    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
-                </ul>
-            `;
+    this.innerHTML = AppBarHtml;
   }
 
   connectedCallback() {
@@ -31,6 +12,4 @@ export default class AppBar extends HTMLElement {
       M.Sidenav.init(elems);
     });
   }
-
-  disconnectedCallback() {}
 }
