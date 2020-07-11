a9eb3595152ea2ae56c0e4ebe00f5cb7cce6c5fa
Anugerah Erlaut <aerlaut@live.com>
2020-07-11 07:22:42 +0700

    dropdown for selecting teams

--- src/js/components/AppContainer.js
+++ src/js/components/AppContainer.js
@@ -1,63 +1,61 @@
-import HomePage from "js/pages/HomePage";
-import SettingsPage from "js/pages/SettingsPage";
-import ClubPage from "js/pages/ClubPage";
-import NotFoundPage from "js/pages/NotFoundPage";
+import HomePage from 'js/pages/HomePage'
+import SettingsPage from 'js/pages/SettingsPage'
+import ClubPage from 'js/pages/ClubPage'
+import NotFoundPage from 'js/pages/NotFoundPage'
 
-customElements.define("home-page", HomePage);
-customElements.define("settings-page", SettingsPage);
-customElements.define("club-page", ClubPage);
-customElements.define("page-not-found", NotFoundPage);
+customElements.define('home-page', HomePage)
+customElements.define('settings-page', SettingsPage)
+customElements.define('club-page', ClubPage)
+customElements.define('page-not-found', NotFoundPage)
 
 export default class AppContainer extends HTMLElement {
   constructor() {
-    super();
+    super()
 
     // Initialize pages
     this.pages = {
       home: new HomePage(),
       settings: new SettingsPage(),
       club: new ClubPage(),
-      "404": new NotFoundPage(),
-    };
+      '404': new NotFoundPage(),
+    }
 
     // Get page from current url
-    let url = window.location.pathname.split("/")[1];
+    let url = window.location.pathname.split('/')[1]
 
-    if (url != "") {
-      this.page = url;
+    if (url != '') {
+      this.page = url
     } else {
-      this.page = "home";
+      this.page = 'home'
     }
   }
 
   connectedCallback() {
     // if page is not set, load dashboard
-
-    console.log(this.page);
-    this.displayPage(this.page);
+    this.displayPage(this.page)
 
     // Handle page changes
     this.addEventListener(
-      "navigate",
+      'navigate',
       (e) => {
-        let splits = e.detail.split("/");
-        let page = splits[splits.length - 1];
+        let splits = e.detail.split('/')
+        let page = splits[splits.length - 1]
 
-        this.displayPage(page);
+        this.displayPage(page)
       },
       false
-    );
+    )
   }
 
   // Route according to the page
   displayPage(page) {
-    this.innerHTML = "";
+    this.innerHTML = ''
 
     if (this.pages.hasOwnProperty(page)) {
-      this.append(this.pages[page]);
+      this.append(this.pages[page])
     } else {
       // 404 Not found
-      this.append(this.pages["404"]);
+      this.append(this.pages['404'])
     }
   }
 }
--- src/js/pages/SettingsPage.js
+++ src/js/pages/SettingsPage.js
@@ -1,12 +1,129 @@
-import SettingsPageLayout from "html/layouts/settings.html";
+import SettingsPageLayout from 'html/layouts/settings.html'
 
 export default class SettingsPage extends HTMLElement {
   constructor() {
-    super();
+    super()
 
-    // Fetch settings page template
-    this.innerHTML = "test";
+    this.data = {
+      options: {
+        leagues: [
+          {
+            id: 2001,
+            name: 'Champions League',
+          },
+          {
+            id: 2002,
+            name: 'Liga Jerman',
+          },
+          {
+            id: 2003,
+            name: 'Liga Belanda',
+          },
+          {
+            id: 2021,
+            name: 'Liga Inggris',
+          },
+          {
+            id: 2014,
+            name: 'Liga Spanyol',
+          },
+          {
+            id: 2015,
+            name: 'Liga Perancis',
+          },
+        ],
+        clubs: {
+          2001: [
+            {
+              id: 1,
+              name: 'A',
+            },
+            {
+              id: 2,
+              name: 'B',
+            },
+            {
+              id: 3,
+              name: 'C',
+            },
+          ],
+          2002: [
+            {
+              id: 1,
+              name: 'A',
+            },
+            {
+              id: 2,
+              name: 'B',
+            },
+            {
+              id: 3,
+              name: 'C',
+            },
+          ],
+          2003: [
+            {
+              id: 1,
+              name: 'A',
+            },
+            {
+              id: 2,
+              name: 'B',
+            },
+            {
+              id: 3,
+              name: 'C',
+            },
+          ],
+        },
+      },
+    }
+
+    this.innerHTML = SettingsPageLayout
   }
 
-  connectedCallback() {}
+  connectedCallback() {
+    // Append options to select
+    const leagueSelect = this.querySelector('#league-select')
+
+    let leagueOptions = ''
+    this.data.options.leagues.forEach((e) => {
+      leagueOptions += `<option value="${e.id}">${e.name}</option>`
+    })
+
+    leagueSelect.innerHTML = leagueOptions
+
+    // Populate clubSelect with first values
+    const clubSelect = this.querySelector('#club-select')
+    let firstLeague = this.data.options.leagues[0]
+    let clubs = this.data.options.clubs[firstLeague.id]
+
+    let clubOptions = ''
+    clubs.forEach((c) => {
+      clubOptions += `<option value="${c.id}">${c.name}</option>`
+    })
+
+    clubSelect.innerHTML = clubOptions
+
+    // Initialize select fields
+    document.addEventListener('DOMContentLoaded', function () {
+      var elems = document.querySelectorAll('select')
+      M.FormSelect.init(elems)
+    })
+
+    // Populate club team when a league is chosen
+    leagueSelect.addEventListener('change', (e) => {
+      let clubs = this.data.options.clubs[e.target.value]
+
+      let clubOptions = ''
+      clubs.forEach((c) => {
+        clubOptions += `<option value="${c.id}">${c.name}</option>`
+      })
+
+      clubSelect.innerHTML = clubOptions
+
+      // Reinitialize clubSelect
+      M.FormSelect.init(clubSelect)
+    })
+  }
 }
