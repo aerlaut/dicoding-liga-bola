43ec1c2985bbe4d15aeddaad161699e0d23f2c17
Anugerah Erlaut <aerlaut@live.com>
2020-07-13 11:31:33 +0700

    layout page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import "lib/css/tailwind.min.css";
 import "lib/css/materialize.min.css";
-import "lib/js/materialize.min.js";
-
 import "css/style.css";
+
+import "lib/js/materialize.min.js";
 
 import AppBar from "js/components/AppBar";
 import AppContainer from "js/components/AppContainer";
